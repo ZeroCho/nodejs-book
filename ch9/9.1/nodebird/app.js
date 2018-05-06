423585e8c4cc705c53be525ac2b6368daf857bd5
zerocho <zerohch0@gmail.com>
2018-05-06 17:57:28 +0900

    Update: ch9.1-ch9.2

---
+++ ch9/9.1/nodebird/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+const pageRouter = require('./routes/page');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8001);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
+app.use(flash());
+
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
---
+++ ch9/9.1/nodebird/routes/page.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird', user: null });
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird', user: null, joinError: req.flash('joinError') });
+});
+
+router.get('/', (req, res, next) => {
+  res.render('main', { title: 'NodeBird', twits: [], user: null, loginError: req.flash('loginError') });
+});
+
+module.exports = router;
