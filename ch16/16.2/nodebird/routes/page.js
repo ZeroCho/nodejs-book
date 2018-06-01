8acc12b1e656af4cadd5e2e4b6a8be919bfb631f
zerocho2 <zerohch1@gmail.com>
2018-06-02 02:16:48 +0900

    Update: ch16

---
+++ ch16/16.2/nodebird/routes/page.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
+
+const router = express.Router();
+
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird', user: req.user });
+});
+
+router.get('/join', isNotLoggedIn, (req, res) => {
+  res.render('join', {
+    title: '회원가입 - NodeBird',
+    user: req.user,
+    joinError: req.flash('joinError'),
+  });
+});
+
+router.get('/', (req, res, next) => {
+  Post.findAll({
+    include: {
+      model: User,
+      attributes: ['id', 'nick'],
+    },
+    order: [['createdAt', 'DESC']],
+  })
+    .then((posts) => {
+      res.render('main', {
+        title: 'NodeBird',
+        twits: posts,
+        user: req.user,
+        loginError: req.flash('loginError'),
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      next(error);
+    });
+});
+
+module.exports = router;
---
+++ ch16/16.3/nodebird/passport/kakaoStrategy.js
@@ -0,0 +1,28 @@
+const KakaoStrategy = require('passport-kakao').Strategy;
+
+const { User } = require('../models');
+
+module.exports = (passport) => {
+  passport.use(new KakaoStrategy({
+    clientID: process.env.KAKAO_ID,
+    callbackURL: '/auth/kakao/callback',
+  }, async (accessToken, refreshToken, profile, done) => {
+    try {
+      const exUser = await User.find({ where: { snsId: profile.id, provider: 'kakao' } });
+      if (exUser) {
+        done(null, exUser);
+      } else {
+        const newUser = await User.create({
+          email: profile._json && profile._json.kaccount_email,
+          nick: profile.displayName,
+          snsId: profile.id,
+          provider: 'kakao',
+        });
+        done(null, newUser);
+      }
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
+  }));
+};
