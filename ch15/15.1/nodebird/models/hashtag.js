9fc0244972474663f34ad1b2a04feba496dedd92
zerocho2 <zerohch1@gmail.com>
2018-06-02 13:34:56 +0900

    Update: charset && collation

--- ch15/15.1/nodebird/models/hashtag.js
+++ ch15/15.1/nodebird/models/hashtag.js
@@ -8,5 +8,7 @@ module.exports = (sequelize, DataTypes) => (
   }, {
     timestamps: true,
     paranoid: true,
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
   })
 );
--- ch16/16.5/nodebird/models/post.js
+++ ch16/16.5/nodebird/models/post.js
@@ -11,5 +11,7 @@ module.exports = (sequelize, DataTypes) => (
   }, {
     timestamps: true,
     paranoid: true,
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
   })
 );
