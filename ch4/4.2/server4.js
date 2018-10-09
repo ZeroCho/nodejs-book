a1abd94de3374a8e3f84bf20cf5c4937f3f8e967
Hyunyoung Cho <zerohch0@gmail.com>
2018-10-09 14:56:43 +0900

    Update server4.js

--- ch4/4.2/server4.js
+++ ch4/4.2/server4.js
@@ -7,7 +7,6 @@ const parseCookies = (cookie = '') =>
   cookie
     .split(';')
     .map(v => v.split('='))
-    .map(([k, ...vs]) => [k, vs.join('=')])
     .reduce((acc, [k, v]) => {
       acc[k.trim()] = decodeURIComponent(v);
       return acc;
