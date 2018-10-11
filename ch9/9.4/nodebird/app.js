5a7279c0a5b6848d0120915e97c69d531e7753a8
wise7034 <wise7034@wise7034ui-MacBook-Pro.local>
2018-10-11 14:11:35 +0900

    샘플코드 오류 수정

--- ch9/9.4/nodebird/app.js
+++ ch9/9.4/nodebird/app.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
@@ -22,6 +23,7 @@ app.set('port', process.env.PORT || 8001);
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -40,6 +42,7 @@ app.use(passport.session());
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
