ed0322d8f8420b915eeaf609bfec15296f7712aa
ZeroCho <zerohch0@gmail.com>
2018-11-30 00:26:32 +0900

    Fix: nodebird (err, req, res, next)

--- ch11/11.6/gif-chat/app.js
+++ ch11/11.6/gif-chat/app.js
@@ -54,10 +54,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
+
 });
 
 const server = app.listen(app.get('port'), () => {
