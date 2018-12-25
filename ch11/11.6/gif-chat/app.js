e64f1c6ec6188a17dee93758841482ed23c1b685
ZeroCho <zerohch0@gmail.com>
2018-12-25 12:44:49 +0900

    Fix: error handler 4 parameters & error.pug

--- ch11/11.6/gif-chat/app.js
+++ ch11/11.6/gif-chat/app.js
@@ -53,8 +53,11 @@ app.use((req, res, next) => {
   next(err);
 });
 
-app.use((err, req, res) => {
-
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 const server = app.listen(app.get('port'), () => {
