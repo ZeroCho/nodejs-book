c2947a374b92d1648ded10153bb5f143b7c8c85e
zerocho1 <zerohch0@gmail.com>
2020-03-02 23:39:11 +0900

    Update: 15

--- ch15/15.1/nodebird/routes/post.js
+++ ch15/15.1/nodebird/routes/post.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const { Post, Hashtag, User } = require('../models');
+const { Post, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
-fs.readdir('uploads', (error) => {
-  if (error) {
-    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('uploads');
-  }
-});
+try {
+  fs.readdir('uploads');
+} catch (error) {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
+}
 
 const upload = multer({
   storage: multer.diskStorage({
@@ -36,44 +36,27 @@ router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
 const upload2 = multer();
 router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
   try {
+    console.log(req.user);
     const post = await Post.create({
       content: req.body.content,
       img: req.body.url,
-      userId: req.user.id,
+      UserId: req.user.id,
     });
     const hashtags = req.body.content.match(/#[^\s#]*/g);
     if (hashtags) {
-      const result = await Promise.all(hashtags.map(tag => Hashtag.findOrCreate({
-        where: { title: tag.slice(1).toLowerCase() },
-      })));
+      const result = await Promise.all(
+        hashtags.map(tag => {
+          return Hashtag.findOrCreate({
+            where: { title: tag.slice(1).toLowerCase() },
+          })
+        }),
+      );
       await post.addHashtags(result.map(r => r[0]));
     }
     res.redirect('/');
   } catch (error) {
     console.error(error);
     next(error);
-  }
-});
-
-router.get('/hashtag', async (req, res, next) => {
-  const query = req.query.hashtag;
-  if (!query) {
-    return res.redirect('/');
-  }
-  try {
-    const hashtag = await Hashtag.findOne({ where: { title: query } });
-    let posts = [];
-    if (hashtag) {
-      posts = await hashtag.getPosts({ include: [{ model: User }] });
-    }
-    return res.render('main', {
-      title: `${query} | NodeBird`,
-      user: req.user,
-      twits: posts,
-    });
-  } catch (error) {
-    console.error(error);
-    return next(error);
   }
 });
 
