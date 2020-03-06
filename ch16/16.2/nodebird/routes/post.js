7f482ac84d8a575d96f574646b511547def78c4a
zerocho1 <zerohch0@gmail.com>
2020-03-07 01:00:54 +0900

    Update: 16.2

--- ch16/16.2/nodebird/routes/post.js
+++ ch16/16.2/nodebird/routes/post.js
@@ -2,6 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
 const { Post, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
@@ -15,22 +17,24 @@ try {
   fs.mkdirSync('uploads');
 }
 
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2',
+});
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'nodebird',
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}${path.basename(file.originalname)}`);
     },
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
 });
-
 router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
   console.log(req.file);
-  res.json({ url: `/img/${req.file.filename}` });
+  res.json({ url: req.file.location });
 });
 
 const upload2 = multer();
--- ch16/16.3/aws-upload/index.js
+++ ch16/16.3/aws-upload/index.js
@@ -17,25 +17,25 @@ exports.handler = (event, context, callback) => {
     console.log('getObj', data);
     return gm(Buffer.from(data.Body))
       .resize(800, 800)
-      .stream(function (err, stdout, stderr) { // 스트림에 출력하는
+      .stream(function (err, stdout, stderr) { // 스트림으로 출력
         if (err) {
           console.error(err);
           return callback(err);
         }
-        const chunks = [];  // 스트림 청크를 모으는위한 배열
+        const chunks = []; // 스트림 청크를 모으기 위한 배열
         stdout.on('data', function (chunk) {
           console.log('pushed');
-          chunks.push(chunk);  // 청크를 모으는
+          chunks.push(chunk); // 청크를 모음
         });
         stdout.on('end', function () {
           console.log('end');
-          const buffer = Buffer.concat(chunks);  // 마지막까지 모아서 저축 경우에 결합하여 버퍼로 내보낼
+          const buffer = Buffer.concat(chunks); // 모은 청크들을 버퍼로 결합
           console.log('buffer', buffer);
           s3.putObject({
             Bucket,
             Key: `thumb/${filename}`,
             Body: buffer,
-          }, function (err, data) {  // S3에 내보낼
+          }, function (err, data) { // S3에 내보낼
             if (err) {
               console.error(err);
               return callback(err);
@@ -46,4 +46,4 @@ exports.handler = (event, context, callback) => {
         });
       });
   });
-}
+};
