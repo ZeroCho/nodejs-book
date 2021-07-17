d0e92ce336648f15baa51cc5838b141cd1c17ed2
Hyunyoung Cho <zerohch0@gmail.com>
2021-07-17 20:33:24 +0900

    Update fsDeletePromise.js

--- ch3/3.6/fsDeletePromise.js
+++ ch3/3.6/fsDeletePromise.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 fs.readdir('./folder')
   .then((dir) => {
     console.log('폴더 내용 확인', dir);
-    return fs.unlink('./folder/newFile.js');
+    return fs.unlink('./folder/newfile.js');
   })
   .then(() => {
     console.log('파일 삭제 성공');
