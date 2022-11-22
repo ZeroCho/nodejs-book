cdc88d8ac30a5dd5f1e7e7701c0555e7ff21fd3d
zerocho <zerohch0+github@gmail.com>
2022-11-23 04:21:15 +0900

    Update: res.locals shorthand

--- ch16/16.3/nodebird/routes/page.js
+++ ch16/16.3/nodebird/routes/page.js
@@ -8,9 +8,9 @@ const router = express.Router();
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
-  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
-  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
-  res.locals.followingIdList = req.user ? req.user.Followings.map(f => f.id) : [];
+  res.locals.followerCount = req.user?.Followers?.length || 0;
+  res.locals.followingCount = req.user?.Followings?.length || 0;
+  res.locals.followingIdList = req.user?.Followings?.map(f => f.id) || [];
   next();
 });
 
