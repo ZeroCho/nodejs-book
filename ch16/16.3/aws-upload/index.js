7412f9ca22563f3b0bc274e77103cef2a236941f
Hyunyoung Cho <zerohch0@gmail.com>
2023-01-06 17:24:05 +0900

    Update index.js

--- ch16/16.3/aws-upload/index.js
+++ ch16/16.3/aws-upload/index.js
@@ -1,32 +1,39 @@
-const AWS = require('aws-sdk');
 const sharp = require('sharp');
+const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 
-const s3 = new AWS.S3();
+const s3 = new S3Client();
 
+// 고양이.png
+// %CD%AE%AW.png
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name;
-  const Key = decodeURIComponent(event.Records[0].s3.object.key);
-  const filename = Key.split('/').at(-1);
-  const ext = Key.split('.').at(-1).toLowerCase();
-  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg 사용합니다.
-  console.log('name', filename, 'ext', ext);
+    const Bucket = event.Records[0].s3.bucket.name;
+    const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/고양이.png
+    const filename = Key.split('/').at(-1);
+    const ext = Key.split('.').at(-1).toLowerCase();
+    const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
+    console.log('name', filename, 'ext', ext);
 
-  try {
-    const s3Object = await s3.getObject({ Bucket, Key }).promise(); // 버퍼로 가져오기
-    console.log('original', s3Object.Body.length);
-    const resizedImage = await sharp(s3Object.Body) // 리사이징
-      .resize(200, 200, { fit: 'inside' })
-      .toFormat(requiredFormat)
-      .toBuffer();
-    await s3.putObject({ // thumb 폴더에 저장
-      Bucket,
-      Key: `thumb/${filename}`,
-      Body: resizedImage,
-    }).promise();
-    console.log('put', resizedImage.length);
-    return callback(null, `thumb/${filename}`);
-  } catch (error) {
-    console.error(error);
-    return callback(error);
-  }
-};
+    try {
+        const getObject = await s3.send(new GetObjectCommand({ Bucket, Key }));
+        const buffers = [];
+        for await (const data of getObject.Body) {
+            buffers.push(data);
+        }
+        const imageBuffer = Buffer.concat(buffers);
+        console.log('put', imageBuffer.length);
+        const resizedImage = await sharp(imageBuffer)
+            .resize(200, 200, { fit: 'inside' })
+            .toFormat(requiredFormat)
+            .toBuffer();
+        await s3.send(new PutObjectCommand({
+            Bucket,
+            Key: `thumb/${filename}`, // thumb/고양이.png
+            Body: resizedImage,
+        }))
+        console.log('put', resizedImage.length);
+        return callback(null, `thumb/${filename}`);
+    } catch (error) {
+        console.error(error);
+        return callback(error);
+    }
+}
