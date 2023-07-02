6f9db6efea50d9706ccf4807023851badd6ec478
zerocho <zerohch0@gmail.com>
2023-07-02 17:14:01 +0900

    Fix: missing js models in 17.2

---
+++ ch17/17.2/nodebird/models/index.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const fs = require('fs');
+const path = require('path');
+const config = require('../config/config')[env];
+
+const db = {};
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
+
+db.sequelize = sequelize;
+
+const basename = path.basename(__filename);
+fs
+  .readdirSync(__dirname) // 현재 폴더의 모든 파일을 조회
+  .filter(file => { // 숨김 파일, 테스트 파일, index.js, js 확장자가 아닌 파일 필터링
+    return (file.indexOf('.') !== 0) && !file.includes('test') && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => { // 해당 파일의 모델 불러와서 init
+    const model = require(path.join(__dirname, file));
+    console.log(file, model.name);
+    db[model.name] = model;
+    model.init(sequelize);
+  });
+
+Object.keys(db).forEach(modelName => { // associate 호출
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+module.exports = db;
---
+++ ch17/17.2/nodebird/models/post.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Post extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      content: {
+        type: Sequelize.STRING(140),
+        allowNull: false,
+      },
+      img: {
+        type: Sequelize.STRING(200),
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'Post',
+      tableName: 'posts',
+      paranoid: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    });
+  }
+
+  static associate(db) {
+    db.Post.belongsTo(db.User);
+    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+  }
+};
