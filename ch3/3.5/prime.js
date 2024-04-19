86aceed522ea571dfab122c67ba7603505077b0a
Hyunyoung Cho <zerohch0@gmail.com>
2024-04-19 23:30:45 +0900

    Update: prime.js
    #801

--- ch3/3.5/prime.js
+++ ch3/3.5/prime.js
@@ -2,10 +2,9 @@ const min = 2;
 const max = 10000000;
 const primes = [];
 
-function findPrimes(start, range) {
+function findPrimes(start, end) {
   let isPrime = true;
-  const end = start + range;
-  for (let i = start; i < end; i++) {
+  for (let i = start; i <= end; i++) {
     for (let j = min; j < Math.sqrt(end); j++) {
       if (i !== j && i % j === 0) {
         isPrime = false;
